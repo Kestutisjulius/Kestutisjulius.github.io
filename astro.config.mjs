32d778b88b67801a51d9351e3ba9be52da0e08b5
Kęstutis Skrebė <kestutis.julius@gmail.com>
2026-03-03 19:46:54 +0200

    blue

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from "astro/config";
 
-// Recommended for GitHub Pages root site:
-// repo name: kestutisjulius.github.io
 export default defineConfig({
-  site: "https://kestutisjulius.github.io",
+  site: "https://ten.vizos.lt",
   output: "static"
 });
